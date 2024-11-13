a77818dfad308b0f1f40244ae21d2beb81e48127
Ezequiel Facundo Rodriguez <rodriguezequiel8@gmail.com>
2024-11-13 11:57:14 -0300

    Suite actualizada

--- Nueva carpeta/src/test/java/testsuite/TestSuite.java
+++ Nueva carpeta/src/test/java/testsuite/TestSuite.java
@@ -4,13 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import modeloDatos.Administrador;
-import modeloDatos.Auto;
-import modeloDatos.ChoferPermanente;
-import modeloDatos.ChoferTemporario;
-import modeloDatos.Combi;
-import modeloNegocio.Empresa;
-import persistencia.EmpresaDTO;
 import prueba.AdministradorTest;
 import prueba.AutoTest;
 import prueba.CalificacionDeChoferTest;
@@ -37,6 +30,10 @@ import pruebaControlador.ControladorTestMetodosBasicos;
 import pruebaControlador.MetodosConMensajesAMostrar;
 import pruebaControlador.MetodosConMensajesAMostrar2;
 import pruebaControlador.MetodosConMensajesAMostrar3;
+import pruebaGUI.GUIAdminConDatosTest;
+import pruebaGUI.GUIAdminVacioTest;
+import pruebaGUI.GUIClienteConDatosTest;
+import pruebaGUI.GUIClienteVacioTest;
 import pruebaGUI.GUIEnabledDisabledAdminTest;
 import pruebaGUI.GUIEnabledDisabledClienteTest;
 import pruebaGUI.GUIEnabledDisabledLogRegTest;
@@ -46,13 +43,42 @@ import pruebaPersistencia.EmpresaDTOTest;
 import pruebaPersistencia.PersistenciaTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({GUIEnabledDisabledLogRegTest.class,GUIEnabledDisabledAdminTest.class,GUIEnabledDisabledClienteTest.class,
-	GUILogRegConDatosTest.class,GUILogRegVacioTest.class,EmpresaDTOTest.class,PersistenciaTest.class,ControladorTestMetodosBasicos.class,
-	MetodosConMensajesAMostrar.class,MetodosConMensajesAMostrar2.class,MetodosConMensajesAMostrar3.class,AdministradorTest.class,
-	AutoTest.class,CalificacionDeChoferTest.class,ChoferPermanenteTest.class,ChoferTemporarioTest.class,CombiTest.class,
-	CrearViajeTest.class,Empresa_AgregarTest.class,EmpresaSinlgetonTest.class,EmpresaTest.class,GetPedidoDeCliente.class,
-	GetUsuarioLogueadotest.class,GetViajeDeClienteTest.class,isAdminTest.class,LoginTest.class,LogOutTest.class,
-	MotoTest.class,PagarYFinalizarTest.class,PedidoTest.class,SetUsuarioLogueadoTest.class,UsuarioTest.class,ViajeTest.class})
+@SuiteClasses({GUIEnabledDisabledLogRegTest.class,
+	GUIEnabledDisabledAdminTest.class,
+	GUIEnabledDisabledClienteTest.class,
+	GUILogRegConDatosTest.class,
+	GUILogRegVacioTest.class,
+	GUIAdminConDatosTest.class,
+	GUIAdminVacioTest.class,
+	GUIClienteConDatosTest.class,
+	GUIClienteVacioTest.class,
+	EmpresaDTOTest.class,
+	PersistenciaTest.class,
+	ControladorTestMetodosBasicos.class,
+	MetodosConMensajesAMostrar.class,
+	MetodosConMensajesAMostrar2.class,
+	MetodosConMensajesAMostrar3.class,
+	AdministradorTest.class,
+	AutoTest.class,
+	CalificacionDeChoferTest.class,
+	ChoferPermanenteTest.class,
+	ChoferTemporarioTest.class,CombiTest.class,
+	CrearViajeTest.class,
+	Empresa_AgregarTest.class,
+	EmpresaSinlgetonTest.class,
+	EmpresaTest.class,
+	GetPedidoDeCliente.class,
+	GetUsuarioLogueadotest.class,
+	GetViajeDeClienteTest.class,
+	isAdminTest.class,
+	LoginTest.class,
+	LogOutTest.class,
+	MotoTest.class,
+	PagarYFinalizarTest.class,
+	PedidoTest.class,
+	SetUsuarioLogueadoTest.class,
+	UsuarioTest.class,
+	ViajeTest.class})
 	
 
 
